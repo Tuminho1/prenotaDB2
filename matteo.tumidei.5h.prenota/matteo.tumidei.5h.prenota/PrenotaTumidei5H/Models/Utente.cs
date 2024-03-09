9243d1e9124ba590459fa59635aa0d422caf4072
Matteo Tumidei <116791499+Tuminho1@users.noreply.github.com>
2024-03-09 09:12:09 +0100

    Add files via upload

---
+++ matteo.tumidei.5h.prenota/matteo.tumidei.5h.prenota/PrenotaTumidei5H/Models/Utente.cs
@@ -0,0 +1,15 @@
+namespace PrenotaTumidei.Models;
+
+public class Utente
+{
+    public string? Nome { get; set; }
+
+    public string? Cognome { get; set; }
+
+    public string? Tipo { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Città { get; set; }
+    
+}
